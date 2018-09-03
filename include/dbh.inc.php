5fe496f7bee82fa8a8af20a3b74a225837de1e53
JM <johnmichaelmaytona@gmail.com>
2018-09-03 23:21:30 +0800

    Connect to database using mysqli

---
+++ include/dbh.inc.php
@@ -0,0 +1,19 @@
+<?php
+
+class Dbh {
+
+    private $servername;
+    private $username;
+    private $password;
+    private $dbname;
+
+    protected function connect() {
+        $this->servername = "localhost";
+        $this->username = "root";
+        $this->password = "";
+        $this->dbname = "number";
+
+        $con = new mysqli($this->servername, $this->username, $this->password, $this->number);
+        return $con;
+    }
+}
